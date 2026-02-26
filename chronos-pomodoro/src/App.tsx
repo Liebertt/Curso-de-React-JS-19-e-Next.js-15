6edad17ffc5f9ece6a993d850e9fc92990a4ec45
Lieberte Ferreira de Sousa <105327109+Liebertt@users.noreply.github.com>
2026-02-26 20:58:28 +0000

    Conceito de Componente e Fragment

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,12 +1,14 @@
-function App() {
+
+
+export function App() {
     console.log('Oi');
-    return 
-    (<div>
+    return     (
+    <> 
     <h1>Olá mundo</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsum, assumenda, iusto vel id et dignissimos nobis vitae praesentium ipsa laboriosam aut maxime neque 
       quibusdam unde qui fugiat, officia esse!
       </p>
-    </div>)
+    </>)
 }
-export default App;
+
 
--- chronos-pomodoro/src/Soma.tsx
+++
@@ -1,5 +0,0 @@
-function Somar(){
-    return 2 + 2;
-}
-
-export default Somar;
