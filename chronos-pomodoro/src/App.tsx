7be7e5692b80fd967acd0a2a9863d7b84fd16a16
Lieberte Ferreira de Sousa <105327109+Liebertt@users.noreply.github.com>
2026-03-03 16:18:40 +0000

    Uso de CSS modules

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,11 +1,14 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
+
 
 export function App() {
     console.log('Oi');
     return     (
     <> 
-    <h1>Olá mundo</h1>
+    <Heading />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsum, assumenda, iusto vel id et dignissimos nobis vitae praesentium ipsa laboriosam aut maxime neque 
       quibusdam unde qui fugiat, officia esse!
       </p>
