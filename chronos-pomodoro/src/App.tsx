04824b25a506280c88588dddc015ff93e245c2c3
Lieberte Ferreira de Sousa <105327109+Liebertt@users.noreply.github.com>
2026-03-03 21:35:38 +0000

    configurando o CSS

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -5,10 +5,12 @@ import './styles/global.css';
 
 
 export function App() {
-    console.log('Oi');
+    
     return     (
     <> 
-    <Heading />
+    <Heading ttr={123} ttr2='string'> Olá mundo 1</Heading>
+        
+        
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsum, assumenda, iusto vel id et dignissimos nobis vitae praesentium ipsa laboriosam aut maxime neque 
       quibusdam unde qui fugiat, officia esse!
       </p>
