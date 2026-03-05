af3beda4e388efb6de504563230f774348ff2f36
Lieberte Ferreira de Sousa <105327109+Liebertt@users.noreply.github.com>
2026-03-05 19:31:10 +0000

    ajustes no heading e noções de tipagem

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -2,15 +2,20 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 
 export function App() {
     
     return     (
     <> 
-    <Heading ttr={123} ttr2='string'> Olá mundo 1</Heading>
-        
-        
+    <Heading > 
+      Olá mundo 1
+
+      <button><TimerIcon /></button>
+     
+      </Heading>
+               
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsum, assumenda, iusto vel id et dignissimos nobis vitae praesentium ipsa laboriosam aut maxime neque 
       quibusdam unde qui fugiat, officia esse!
       </p>
